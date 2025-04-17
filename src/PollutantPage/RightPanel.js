a37843c5b6cc643d665c2aa77948289ffe4abee6
arhammxo <anas.21bce7711@vitapstudent.ac.in>
2025-04-17 12:22:34 +0530

    8 crore changes

--- src/PollutantPage/RightPanel.js
+++ src/PollutantPage/RightPanel.js
@@ -42,10 +42,21 @@ const RightPanel = ({ sections = [] }) => {
     }
   ];
 
-  const scrollToId = (id) => {
+  const scrollToId = (id, specificOffset) => {
     const element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
+      const defaultOffset = 77;
+      const offset = specificOffset !== undefined ? specificOffset : defaultOffset;
+
+      const bodyRect = document.body.getBoundingClientRect().top;
+      const elementRect = element.getBoundingClientRect().top;
+      const elementPosition = elementRect - bodyRect;
+      const offsetPosition = elementPosition + offset;
+
+      window.scrollTo({
+         top: offsetPosition,
+         behavior: "smooth"
+      });
     } else {
       console.warn(`Element with ID "${id}" not found.`);
     }
@@ -119,7 +130,7 @@ const RightPanel = ({ sections = [] }) => {
                 <p className="plantDetails">
                   {plantDetails}
                 </p>
-                <KnowMoreButton className="knowMoreButtonRightPanel" onClick={() => scrollToId('plant-name')} />
+                <KnowMoreButton className="knowMoreButtonRightPanel" onClick={() => scrollToId('plant-name', 130)} />
               </div>
             </div>
           </main>
